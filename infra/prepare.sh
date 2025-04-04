ec510d7d420fb75d39ef5c6aff36496f5accdd20
mark-anthony-libres <codesomia@gmail.com>
2025-04-04 11:13:37 +0800

    debian compatible

---
+++ infra/prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Delete all files and subfolders in the output folder
+echo "======= Deleting all items in the output folder... ======="
+rm -rf ./output/*  # Delete files recursively
+rmdir ./output/    # Remove the output directory
+
+# Recreate the output folder
+mkdir ./output
+
+# Ensure the temp_scripts folder exists
+echo "creating temp_scripts folder"
+mkdir ./output/temp_scripts
+
+# Copy the debug.bat to the output folder
+cp ./debug.bat ./output/
+
+echo "APP_PROJECT = $APP_PROJECT"
